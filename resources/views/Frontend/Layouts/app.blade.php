59ec6478693552164e37bf2d74d37b480c4faa21
BIPLOB <developerbiplob.bd@gmail.com>
2022-04-02 12:08:36 +0600

    Frontend Setup Done

---
+++ resources/views/Frontend/Layouts/app.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>@yield('title', 'Blog Project 2022')</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="robots" content="all,follow">
+        <!-- Bootstrap CSS-->
+        <link rel="stylesheet" href="{{ asset('Frontend') }}/vendor/bootstrap/css/bootstrap.min.css">
+        <!-- Font Awesome CSS-->
+        <link rel="stylesheet" href="{{ asset('Frontend') }}/vendor/font-awesome/css/font-awesome.min.css">
+        <!-- Custom icon font-->
+        <link rel="stylesheet" href="{{ asset('Frontend') }}/css/fontastic.css">
+        <!-- Google fonts - Open Sans-->
+
+        <link rel="stylesheet" href="{{ asset('Frontend') }}/css/owl.carousel.min.css">
+        <link rel="stylesheet" href="{{ asset('Frontend') }}/css/animate.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+        <!-- Fancybox-->
+        <link rel="stylesheet" href="{{ asset('Frontend') }}/vendor/@fancyapps/fancybox/jquery.fancybox.min.css">
+        <!-- theme stylesheet-->
+        <link rel="stylesheet" href="{{ asset('Frontend') }}/css/style.default.css" id="theme-stylesheet">
+        <!-- Custom stylesheet - for your changes-->
+        <link rel="stylesheet" href="{{ asset('Frontend') }}/css/custom.css">
+        <!-- Favicon-->
+        <link rel="shortcut icon" href="{{ asset('Frontend') }}/favicon.png">
+
+    </head>
+    <body>
+        @yield('app-content')
+        <!-- JavaScript files-->
+        <script src="{{ asset('Frontend') }}/vendor/jquery/jquery.min.js"></script>
+        <script src="{{ asset('Frontend') }}/vendor/popper.js/umd/popper.min.js"> </script>
+        <script src="{{ asset('Frontend') }}/vendor/bootstrap/js/bootstrap.min.js"></script>
+        <script src="{{ asset('Frontend') }}/vendor/jquery.cookie/jquery.cookie.js"> </script>
+        <script src="{{ asset('Frontend') }}/vendor/@fancyapps/fancybox/jquery.fancybox.min.js"></script>
+        <script src="{{ asset('Frontend') }}/js/owl.carousel.min.js"></script>
+        <script src="{{ asset('Frontend') }}/js/wow.min.js"></script>
+        <script src="{{ asset('Frontend') }}/js/front.js"></script>
+        <script src="{{ asset('Frontend') }}/js/custom.js"></script>
+    </body>
+</html>
---
+++ resources/views/Frontend/partials/slider.blade.php
@@ -0,0 +1,38 @@
+<div class="owl-carousel hero_slider">
+    <section style="background: url({{ asset('Frontend') }}/img/hero.jpg); background-size: cover; background-position: center center" class="hero wow animate__animated animate__fadeInTopLeft">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-7">
+                    <h1 class="bg-dark p-3">Bootstrap 4 Blog - A free template by Bootstrap Temple</h1><a href="#" class="hero-link">Discover More</a>
+                </div>
+            </div><a href=".intro" class="continue link-scroll"><i class="fa fa-long-arrow-down"></i> Scroll Down</a>
+        </div>
+    </section>
+    <section style="background: url({{ asset('Frontend') }}/img/blog-2.jpg); background-size: cover; background-position: center center" class="hero wow animate__animated animate__zoomIn" data-wow-delay=".3s">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-7">
+                    <h1 class="bg-dark p-3">Bootstrap 4 Blog - A free template by Bootstrap Temple</h1><a href="#" class="hero-link">Discover More</a>
+                </div>
+            </div><a href=".intro" class="continue link-scroll"><i class="fa fa-long-arrow-down"></i> Scroll Down</a>
+        </div>
+    </section>
+    <section style="background: url(img/hero2.jpg); background-size: cover; background-position: center center" class="hero wow animate__animated animate__zoomIn" data-wow-delay=".3s">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-7">
+                    <h1 class="bg-dark p-3">Bootstrap 5 Blog - Anisur Rahaman Shahin Temple</h1><a href="#" class="hero-link">Discover More</a>
+                </div>
+            </div><a href=".intro" class="continue link-scroll"><i class="fa fa-long-arrow-down"></i> Scroll Down</a>
+        </div>
+    </section>
+    <section style="background: url({{ asset('Frontend') }}/img/hero3.jpg); background-size: cover; background-position: center center" class="hero">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-7">
+                    <h1 class="bg-dark p-3" >A free template by Bootstrap Temple</h1><a href="#" class="hero-link">Discover More</a>
+                </div>
+            </div><a href=".intro" class="continue link-scroll"><i class="fa fa-long-arrow-down"></i> Scroll Down</a>
+        </div>
+    </section>
+</div>
